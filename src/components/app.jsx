a2367153af65784febf6b90e7fc8c4ed41f825ed
Nessiah919 <nesmith.charlesii@gmail.com>
2021-06-27 14:20:57 -0400

    Created App component an index.js. imported App component to index.js

---
+++ src/components/app.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+//import axios from 'axios';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>TESTING</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
