cdc7796802a1a77e463db9cde5c32291cac31708
Nessiah919 <nesmith.charlesii@gmail.com>
2021-06-28 16:37:02 -0400

    created object 'input' in Submittal function of regForm.jsx to hold all fields of form in dictionary. added if statement to Submittal function to validate password and confirm password match

---
+++ src/components/CustomHook/customForm.jsx
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+const CustomForm = (callback) => {
+    const [inputs, setInputs] = useState({});
+    
+    const handleChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}))
+    }
+
+    const handleSubmit = (event) => {
+        if(event) {
+            event.preventDefault();
+        }
+        callback()
+    }
+
+    return {
+        handleChange,
+        handleSubmit,
+        inputs
+    }
+}
+
+export default CustomForm;
--- src/components/RegForm/regForm.jsx
+++ src/components/RegForm/regForm.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
+import CustomForm from '../CustomHook/customForm';
+import axios from 'axios';
 import './regForm.css';
 
 const RegForm = (props) => {
+
+    const Submittal = () => {
+        let input = {
+            firstName: inputs.firstName,
+            lastName: inputs.lastName,
+            userName: inputs.userName,
+            email: inputs.email,
+            password: inputs.password,
+            confirmPW: inputs.confirmPW,
+        }
+        if (inputs.password !== inputs.confirmPW) {
+            alert("Confirm password and password must match")
+        } else {
+            console.log('PASSWORDS MATCH', input);
+        }
+    }
+
+    const {handleChange, handleSubmit, inputs} = CustomForm(Submittal)
+
     return (
         <div className="regForm-container my-5">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="firstName">First Name:</label>
-                    <input className="form-control" type="text" name="firstName" />
+                    <input className="form-control" type="text" name="firstName" onChange={handleChange} value={inputs.firstName}/>
                     <label htmlFor="lastName">Last Name:</label>
-                    <input className="form-control" type="text" name="lastName" />
+                    <input className="form-control" type="text" name="lastName" onChange={handleChange} value={inputs.lastName}/>
                     <label htmlFor="userName">Username:</label>
-                    <input className="form-control" type="text" name="userName" />
+                    <input className="form-control" type="text" name="userName" onChange={handleChange} value={inputs.userName}/>
                     <label htmlFor="email">Email:</label>
-                    <input className="form-control" type="email" name="email" />
+                    <input className="form-control" type="email" name="email" onChange={handleChange} value={inputs.email}/>
                     <label htmlFor="password">Password:</label>
-                    <input className="form-control" type="text" name="password" />
+                    <input className="form-control" type="text" name="password" onChange={handleChange} value={inputs.password}/>
                     <label htmlFor="confirmPW">Confirm Password:</label>
-                    <input className="form-control" type="text" name="confirmPW" />
+                    <input className="form-control" type="text" name="confirmPW" onChange={handleChange} value={inputs.confirmPW}/>
                     <br/>
                     <button className="confirmReg">Sign Up!</button>
                 </div>
