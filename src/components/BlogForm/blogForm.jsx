21e6004ff6bea474a5db9d15dff34aa6e259f49d
Nessiah919 <nesmith.charlesii@gmail.com>
2021-07-15 18:08:27 -0400

    changed blogForm.jsx to class component. added state variable 'header_image'.

--- src/components/BlogForm/blogForm.jsx
+++ src/components/BlogForm/blogForm.jsx
@@ -1,68 +1,91 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CustomForm from '../CustomHook/customForm';
 import '../fontawesome/css/all.min.css';
 import axios from 'axios';
 import './blogForm.css';
 
-const BlogForm = (props) => {
+class BlogForm extends Component {
 
-    const Submittal = () => {
-        const blog = {
-            //Keys must match that of the model from django exactly!
-            title: inputs.title,
-            content: inputs.content,
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            content: "",
+            header_image: null
         }
-        console.log('Blog Dict', blog)
-        blogSubmittal(blog)
     }
 
-    const blogSubmittal = async(blog) => {
-        console.log('Blog submit token', localStorage.getItem('token'))
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleImageChange = e => {
+        this.setState({
+            upload: e.target.files[0]
+        }, () => {console.log('HEADER IMAGE STATE', this.state.header_image)})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        let formData = new FormData()
+        formData.append('header_image', this.state.header_image, this.state.header_image.name)
+        formData.append('title', this.state.title)
+        formData.append('content', this.state.content)
+        console.log('UPLOAD STATE NAME', this.state.upload.name)
+        this.blogSubmittal(formData)
+    }
+
+    blogSubmittal = async(formData) => {
+        console.log('BLOG FORM DATA AT API', formData)
         try {
             let token = localStorage.getItem('token');
-            let config = {headers: { Authorization: `JWT ${token}` }};
-            let {data} = await axios.post(`http://127.0.0.1:8000/api/blog_content_creator/`, blog, config);
+            let config = {headers: { Authorization: `JWT ${token}`}, 'content-type': 'multipart/form-data'};
+            let {data} = await axios.post(`http://127.0.0.1:8000/api/blog_content_creator/`, formData, config);
             console.log('BLOG DATA', data)
         }
         catch(error) {
             alert(`Whoops! ${error} Looks like we're having some technical difficulties. Try again later`)
         }
     }
-
-    const {handleChange, handleSubmit, inputs} = CustomForm(Submittal)
-
-    return (
-        <div className="blogForm-container my-5">
-            <form onSubmit={handleSubmit}>
-                <h2>Share Your Knowledge</h2>
-                <div className="form-group">
-                    <label htmlFor="title">Title:</label>
-                    <input className="form-control" type="text" name="title" onChange={handleChange} value={inputs.title}/>
-                    <div className="text-tools">
-                        <i class="fas fa-bold"></i>
-                        <i class="fas fa-italic"></i>
-                        <i class="fas fa-align-left"></i>
-                        <i class="fas fa-align-center"></i>
-                        <i class="fas fa-align-right"></i>
-                        <i class="fas fa-underline"></i>
-                        <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Uppercase text">Upper Case</button>
-                        <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Lowercase text">Lower Case</button>
-                        <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Capitalize text">Capitalize</button>
-                        <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Clear text">Clear Text</button>
+    render() {
+        return (
+            <div className="blogForm-container my-5">
+                <form onSubmit={this.handleSubmit}>
+                    <h2>Share Your Knowledge</h2>
+                    <div className="form-group">
+                        <label htmlFor="title">Title:</label>
+                        <input className="form-control" type="text" name="title" onChange={this.handleChange} value={this.state.title}/>
+                        <div className="text-tools">
+                            <i class="fas fa-bold"></i>
+                            <i class="fas fa-italic"></i>
+                            <i class="fas fa-align-left"></i>
+                            <i class="fas fa-align-center"></i>
+                            <i class="fas fa-align-right"></i>
+                            <i class="fas fa-underline"></i>
+                            <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Uppercase text">Upper Case</button>
+                            <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Lowercase text">Lower Case</button>
+                            <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Capitalize text">Capitalize</button>
+                            <button className="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Clear text">Clear Text</button>
+                        </div>
+                        <label htmlFor="content">Content:</label>
+                        <textarea className="form-control" type="text" name="content" onChange={this.handleChange} value={this.state.content}/>
+                        <br/>
+                        <button className="confirmReg">Submit!</button>
                     </div>
-                    <label htmlFor="content">Content:</label>
-                    <textarea className="form-control" type="text" name="content" onChange={handleChange} value={inputs.content}/>
-                    <br/>
-                    <button className="confirmReg">Submit!</button>
-                </div>
-            </form>
-            <div className="article-display">
-                <textarea id="display-text">
+                </form>
+                <div className="article-display">
+                    <div className="header-image">
 
-                </textarea>
+                    </div>
+                    <div className="display-text">
+                        
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default BlogForm;
