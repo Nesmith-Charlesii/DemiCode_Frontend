94df06c798c4dae03d7d41d79434a62bf290b8c6
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-31 23:04:50 -0400

    added all product information to product display container. Will fix layout

--- src/components/ProductDisplay/productDisplay.jsx
+++ src/components/ProductDisplay/productDisplay.jsx
@@ -17,17 +17,30 @@ const ProductDisplay = (props) => {
             let config = {headers: { Authorization: `JWT ${token}`}};
             let {data} = await axios.get(`http://127.0.0.1:8000/api/digital_product/18`, config)
             setProduct(data)
-            console.log(product);
+            console.log(data)
         }
         catch(error) {
             alert(`Whoops! Looks like we're having some technical difficulties. Try again later`)
         }
     }
 
+    const baseURL = 'http://127.0.0.1:8000'
+
     return (
         <div className="product-display-container">
-            <div className="product-title">
-                <h1>Test</h1>
+            <div className="product-info">
+                <div className="product-title">
+                    <h1>{product.name}</h1>
+                </div>
+                <div className="product-description">
+                    <h4>{product.description}</h4>
+                </div>
+                <div className="product-image">
+                    <img id="product-imgage" src={baseURL + product.image} alt="product" style={{height: "350px"}}/>
+                </div>
+                <div className="product-price">
+                    <p>${product.price}</p>
+                </div>
             </div>
         </div>
     )
