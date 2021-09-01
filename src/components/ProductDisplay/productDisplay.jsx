024e86848c51adc7ac94017976a88fbfd8f84513
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-31 22:39:47 -0400

    created 'getProduct' function to make GET request for specified product. Test successful. useEffect calls getProduct on page load

--- src/components/ProductDisplay/productDisplay.jsx
+++ src/components/ProductDisplay/productDisplay.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './productDisplay.css';
 
 const ProductDisplay = (props) => {
+
+    const [product, setProduct] = useState(null); 
+
+    useEffect(() => {
+        getProduct()
+    }, [])
+
+    const getProduct = async() => {
+        console.log('retrieving product')
+        try {
+            let token = localStorage.getItem('token');
+            let config = {headers: { Authorization: `JWT ${token}`}};
+            let {data} = await axios.get(`http://127.0.0.1:8000/api/digital_product/18`, config)
+            setProduct(data)
+            console.log(product);
+        }
+        catch(error) {
+            alert(`Whoops! Looks like we're having some technical difficulties. Try again later`)
+        }
+    }
+
     return (
         <div className="product-display-container">
-            <p>Test</p>
+            <div className="product-title">
+                <h1>Test</h1>
+            </div>
         </div>
     )
 }
